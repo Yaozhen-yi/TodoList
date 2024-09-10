c0a447966e798d0ec50dd3f56f3f28eb86a7f7ff
Yaozhen-yi <a021560@gmail.com>
2024-09-10 20:47:32 +0800

    測試

--- vite.config.js
+++ vite.config.js
@@ -4,15 +4,29 @@ import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-  ],
-  base: '/todolist/',
-  // base: process.env.VITE_APP_BASE_URL || '/todolist_railway/'|| '/', // 从环境变量获取 base
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+export default defineConfig(({ mode }) => {
+  // 加载环境变量
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    plugins: [
+      vue(),
+    ],
+    base: '/',
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
+    },
+    // 根據動態環境設置其他配置
+    server: {
+      proxy: {
+        '/api': {
+          target: env.VITE_APP_API_BASE_URL,
+          changeOrigin: true,
+          secure: false,
+        }
+      }
     }
   }
 })
