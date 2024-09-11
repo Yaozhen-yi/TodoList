a6989f1c3bfe37b041718a9a3c41caa8034107f3
Yaozhen-yi <a021560@gmail.com>
2024-09-11 15:08:57 +0800

    前後端分離

---
+++ vite.config.js
@@ -0,0 +1,32 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig, loadEnv } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => {
+  // 加载环境变量
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    plugins: [
+      vue(),
+    ],
+    base: '/',
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
+    },
+    // 根據動態環境設置其他配置如果需要，还可以根据环境动态设置其他配置
+    server: {
+      proxy: mode === 'production' ? {
+        '/api': {
+          target: env.VITE_APP_API_BASE_URL , // 确保代理指向正确的目标
+          changeOrigin: true,
+          secure: false,
+        }
+      } : undefined // 生产环境不需要代理
+  },
+};
+})
