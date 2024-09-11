020d307b1eb6e708ec76d4e18bc87764af967b53
Yaozhen-yi <a021560@gmail.com>
2024-09-11 15:42:57 +0800

    測試前後方離

--- deploy.sh
+++ deploy.sh
@@ -8,5 +8,5 @@ cd dist
 git init
 git add -A
 git commit -m 'deploy'
-git push -f https://github.com/Yaozhen-yi/todolist_railway.git master:gh-pages
+git push -f https://github.com/Yaozhen-yi/todolist.git master:gh-pages
 cd -
